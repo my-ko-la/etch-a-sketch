b06641f942d9ec5ac440268bf4d212f86662510b
vvarhand <63235046+vvarhand@users.noreply.github.com>
2022-06-22 12:52:16 +0200

    Fix GAMERMORE and REFRESH buttons
    Buttons now work as intended. In the previous version the
    EventListeners were not being removed by the REFRESH BUTTON as well
    as by the buttons themselves.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const body = document.querySelector('body')
 const gridContainer = document.getElementById('gridContainer');
+const rainbowColorsHex = [
+    "#9400D3",
+    "#4B0082",
+    "#0000FF",
+    "#00FF00",
+    "#FFFF00",
+    "#FF7F00",
+    "#FF0000"
+];
+let startingGrid = 16;
 
-let startingGrid = 4;
+let BRUSH;
+
+
 
 const updateGrid = (user) =>
 {
@@ -17,6 +29,8 @@ const updateGrid = (user) =>
     }
 }
 
+const gridDivs = document.querySelectorAll('.grid-div');
+
 const activateDivs = () =>
 {
     const gridDivs = document.querySelectorAll('.grid-div');
@@ -24,27 +38,60 @@ const activateDivs = () =>
     {
         e.target.classList.add('grid-on-hover');
     }));    
+    console.log('activated');
 }
 
 updateGrid(startingGrid);
 activateDivs();
 
+const ApplyColor_gaming = (e) =>
+{
+    e.target.classList.remove('grid-on-hover', 'rainbow');
+    e.target.classList.add('gaming');
+}
+
+const ApplyColor_rainbow = (e) =>
+{
+    e.target.classList.remove('grid-on-hover', 'gaming');
+    e.target.classList.add('rainbow');
+}
 
 
 // MENU -----------------------------------------------------------------------
 const thisGuyIsGaming = () =>
 {
     const gridDivs = document.querySelectorAll('.grid-div');
-    gridDivs.forEach(div => div.addEventListener('mouseover', e =>
-    {
-        e.target.classList.add('gaming');
-    }))
+    const gamerBtn = document.getElementById('gamermode');
+    gamerBtn.classList.toggle('active-btn');
+    if (gamerBtn.classList.contains('active-btn')) {
+        gridDivs.forEach(div => div.addEventListener('mouseover', ApplyColor_gaming));
+    } else if (!gamerBtn.classList.contains('active-btn')) {
+        gridDivs.forEach(div => div.removeEventListener('mouseover', ApplyColor_gaming));
+    }
 }
 
-const gamerBtn = document.getElementById('gamermode');
-gamerBtn.onclick = thisGuyIsGaming();
+const refreshDivsBG = () =>
+{
+    const gridDivs = document.querySelectorAll('.grid-div');
+    gridDivs.forEach(div => div.classList.remove('gaming', 'rainbow', 'grid-on-hover'));
+    document.querySelectorAll('button').forEach(btn => btn.classList.remove('active-btn'));
+    gridDivs.forEach(div => div.removeEventListener('mouseover', ApplyColor_gaming));
+    activateDivs();
+}
 
+const rainbowStyle = () =>
+{
+    const gridDivs = document.querySelectorAll('.grid-div');
+    document.getElementById('rainbow').classList.toggle('active-btn');
+    gridDivs.forEach(div => div.addEventListener('mouseover', e =>
+    {
+        let randomNum = (Math.floor(Math.random() * 10)) % 7;
+        e.target.remove('grid-on-hover', 'gaming');
+        let color = rainbowColorsHex[randomNum];
+        e.target.style.backgroundColor = `${color}`;
+    }));
 
+}
 // SLIDER ----------------------------------------------------------------------
 const slider = document.getElementById('slider-id');
 const sqnum = document.getElementById('sqnum');
